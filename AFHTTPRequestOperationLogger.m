898f4e7c40f20f01b2ffb7436d78187065115fcd
Mattt Thompson <m@mattt.me>
2011-12-06 13:00:40 -0600

    Initial import

---
+++ AFHTTPRequestOperationLogger.m
@@ -0,0 +1,124 @@
+// AFHTTPRequestLogger.h
+//
+// Copyright (c) 2011 Mattt Thompson (http://mattt.me)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#import "AFHTTPRequestOperationLogger.h"
+#import "AFHTTPRequestOperation.h"
+
+@implementation AFHTTPRequestOperationLogger
+@synthesize level = _level;
+
++ (AFHTTPRequestOperationLogger *)sharedLogger {
+    static AFHTTPRequestOperationLogger *_sharedLogger = nil;
+
+    if (!_sharedLogger) {
+        @synchronized(self) {
+            _sharedLogger = [[self alloc] init];
+        }
+    }
+
+  return _sharedLogger;
+}
+
+- (id)init {
+    self = [super init];
+    if (!self) {
+        return nil;
+    }
+
+    self.level = AFLoggerLevelInfo;
+
+    return self;
+}
+
+- (void)dealloc {
+  [self stopLogging];
+
+  [super dealloc];
+}
+
+- (void)startLogging {
+  [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(HTTPOperationDidStart:) name:AFNetworkingOperationDidStartNotification object:nil];
+  [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(HTTPOperationDidFinish:) name:AFNetworkingOperationDidFinishNotification object:nil];
+}
+
+- (void)stopLogging {
+  [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+#pragma mark - NSNotification
+
+- (void)HTTPOperationDidStart:(NSNotification *)notification {
+  AFHTTPRequestOperation *operation = (AFHTTPRequestOperation *)[notification object];
+
+    if (![operation isKindOfClass:[AFHTTPRequestOperation class]]) {
+        return;
+    }
+
+  NSString *body = nil;
+  if ([operation.request HTTPBody]) {
+    body = [NSString stringWithUTF8String:[[operation.request HTTPBody] bytes]];
+  }
+
+  switch (self.level) {
+    case AFLoggerLevelDebug:
+      NSLog(@"%@ '%@': %@ %@", [operation.request HTTPMethod], [[operation.request URL] absoluteString], [operation.request allHTTPHeaderFields], body);
+      break;
+    case AFLoggerLevelInfo:
+      NSLog(@"%@ '%@'", [operation.request HTTPMethod], [[operation.request URL] absoluteString]);
+      break;
+        default:
+            break;
+  }
+}
+
+- (void)HTTPOperationDidFinish:(NSNotification *)notification {
+  AFHTTPRequestOperation *operation = (AFHTTPRequestOperation *)[notification object];
+
+    if (![operation isKindOfClass:[AFHTTPRequestOperation class]]) {
+        return;
+    }
+
+    if (operation.error) {
+        switch (self.level) {
+            case AFLoggerLevelDebug:
+            case AFLoggerLevelInfo:
+            case AFLoggerLevelWarn:
+            case AFLoggerLevelError:
+                NSLog(@"[Error] %@ '%@' (%ld): %@", [operation.request HTTPMethod], [[operation.response URL] absoluteString], [operation.response statusCode], operation.error);
+            default:
+                break;
+        }
+    } else {
+        switch (self.level) {
+            case AFLoggerLevelDebug:
+                NSLog(@"%ld '%@': %@", [operation.response statusCode], [[operation.response URL] absoluteString], operation.responseString);
+                break;
+            case AFLoggerLevelInfo:
+                NSLog(@"%ld '%@'", [operation.response statusCode], [[operation.response URL] absoluteString]);
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+@end
---
+++ README.md
@@ -0,0 +1,38 @@
+# AFHTTPRequestOperationLogger
+
+AFHTTPRequestOperationLogger is an extension for [AFNetworking](http://github.com/AFNetworking/AFNetworking/) that logs HTTP requests as they are sent and received.
+
+This is still in early stages of development, so proceed with caution when using this in a production application. Any bug reports, feature requests, or general feedback at this point would be greatly appreciated.
+
+## Example Usage
+
+Add the following code to `AppDelegate.m -application:didFinishLaunchingWithOptions:`:
+
+``` objective-c
+[[AFNetworkLogger sharedLogger] startLogging];
+```
+
+Now all `AFHTTPRequestOperation` will have their request and response logged to the console, a la:
+
+```
+GET http://example.com/foo/bar.json
+200 http://example.com/foo/bar.json
+```
+
+If the default logging level is too verbose—say, if you only want to know when requests fail, then changing it is as simple as:
+
+``` objective-c
+[[AFNetworkLogger sharedLogger] setLevel:AFLoggerLevelError];
+```
+
+## Contact
+
+Mattt Thompson
+
+- http://github.com/mattt
+- http://twitter.com/mattt
+- m@mattt.me
+
+## License
+
+AFHTTPRequestOperationLogger is available under the MIT license. See the LICENSE file for more info.
